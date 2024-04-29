996ab725e7dc42e2a4e7cd51e2aff662e4980897
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-04-29 17:54:59 -0300

    feat: add confetti for complete phrase correctly

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { usePhrasesStore } from "../stores/Phrases";
+import JSConfetti from 'js-confetti';
+// import TimerGame from "./TimerGame";
 
 const Phrase = () => {
+  
   const fetchPhrases = usePhrasesStore((state) => state.fetchPhrases);
   const phrases = usePhrasesStore((state) => state.phrases);
   const setTextColor = usePhrasesStore((state) => state.setTextColor);
@@ -11,7 +14,7 @@ const Phrase = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await fetchPhrases(5);
+        await fetchPhrases(1);
       } catch (error) {
         console.error("Error fetching phrases:", error);
       }
@@ -19,47 +22,66 @@ const Phrase = () => {
     fetchData();
   }, []);
 
-  const handleChange = (e: { target: { value: any; }; }) => {
+  const handleChange = (e: { target: { value: string; }; }) => {
     const inputValue = e.target.value;
-    const newTypedLetters = inputValue.split("").map((letter: string, index: string | number) => ({
+    const newTypedLetters = inputValue.split("").map((letter, index) => ({
       letter,
-      correct: letter === phrases[0].texto[index],
+      correct: letter === phrases[0]?.texto[index],
     }));
 
     usePhrasesStore.setState({ typedLetters: newTypedLetters });
 
     const allCorrect = newTypedLetters.every((letterObj) => letterObj.correct);
+
     if (allCorrect) {
       setTextColor("#ffff");
+
+      const jsConfetti = new JSConfetti()
+
+      if (inputValue.length === phrases[0]?.texto.length) {
+        jsConfetti.addConfetti()
+        setTimeout(() => {
+          fetchPhrases(1);
+          e.target.value = "";
+          usePhrasesStore.setState({ typedLetters: [] });
+        }, 3000);
+
+      }
+    } else {
+      setTextColor("");
     }
   };
 
   return (
-    <div>
-      <p className="instructions" style={{ color: phrase.textColor }}>
-        {phrases.length > 0 &&
-          phrases[0].texto.split("").map((letter, index) => (
-            <span
-              key={index}
-              style={{ color: typedLetters[index]?.correct ? "white" : "#f3ec78" }}
-            >
-              {letter}
-            </span>
-          ))}
-      </p>
-      <section>
-        <li className="link-input">
-          <label htmlFor="Write phrase">
-            <h2>Write Phrase</h2>
-            <input
-              onChange={handleChange}
-              type="text"
-              placeholder="Type the phrase without making a mistake 👆"
-            />
-          </label>
-        </li>
-      </section>
+    <>
+    {/* <TimerGame /> */}
+      <div>
+        <p className="instructions" style={{ color: phrase.textColor }}>
+          {phrases.length > 0 &&
+            phrases[0]?.texto.split("").map((letter, index) => (
+              <span
+                key={index}
+                style={{ color: typedLetters[index]?.correct ? "white" : "#f3ec78" }}
+              >
+                {letter}
+              </span>
+            ))}
+        </p>
+        <section>
+          <li className="link-input">
+            <label htmlFor="Write phrase">
+              <h2>Write Phrase</h2>
+              <input
+                onChange={handleChange}
+                type="text"
+                placeholder="Type the phrase without making a mistake 👆"
+              />
+            </label>
+          </li>
+        </section>
     </div>
+    </>
+
   );
 };
 
