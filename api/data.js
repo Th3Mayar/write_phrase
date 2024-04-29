181c8467dcfadcb2181478880a25d33a51dbd74d
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-04-29 17:46:55 -0300

    feat: add type correct and fetch data by data.js

---
+++ api/data.js
@@ -0,0 +1,135 @@
+export const questionPlay = [
+    {
+        id: 1,
+        texto: "La tecnología avanza a pasos agigantados, transformando nuestra forma de vivir y trabajar. La inteligencia artificial, en particular, está revolucionando múltiples industrias al ofrecer soluciones innovadoras y eficientes.",
+        autor: "John Smith",
+        nombre: "Avance Tecnológico",
+        textColor: "white"
+    },
+    {
+        id: 2,
+        texto: "El cambio climático es uno de los desafíos más urgentes que enfrenta la humanidad. Con el aumento de las temperaturas y los fenómenos meteorológicos extremos, es crucial tomar medidas para mitigar sus efectos y proteger nuestro planeta para las generaciones futuras.",
+        autor: "María López",
+        nombre: "Crisis Climática",
+        textColor: "white"
+    },
+    {
+        id: 3,
+        texto: "La globalización ha conectado a personas de todo el mundo como nunca antes. Esta interconexión ha traído consigo beneficios económicos y culturales, pero también desafíos en términos de desigualdad y preservación de la identidad cultural.",
+        autor: "Ahmed Khan",
+        nombre: "Globalización",
+        textColor: "white"
+    },
+    {
+        id: 4,
+        texto: "La pandemia de COVID-19 ha impactado profundamente en todos los aspectos de la vida. Desde la salud pública hasta la economía global, esta crisis ha puesto a prueba la capacidad de adaptación y resiliencia de las sociedades en todo el mundo.",
+        autor: "Anna García",
+        nombre: "COVID-19",
+        textColor: "white"
+    },
+    {
+        id: 5,
+        texto: "El emprendimiento es fundamental para impulsar la innovación y el crecimiento económico. Los emprendedores son agentes de cambio que desafían lo establecido y crean nuevas oportunidades en el mercado.",
+        autor: "Carlos Martínez",
+        nombre: "Emprendimiento",
+        textColor: "white"
+    },
+    {
+        id: 6,
+        texto: "El desarrollo sostenible busca satisfacer las necesidades actuales sin comprometer la capacidad de las futuras generaciones para satisfacer las suyas. Es un enfoque integral que aborda aspectos económicos, sociales y ambientales.",
+        autor: "Luisa Fernández",
+        nombre: "Desarrollo Sostenible",
+        textColor: "white"
+    },
+    {
+        id: 7,
+        texto: "La inteligencia emocional es clave para el éxito personal y profesional. Nos permite entender y manejar nuestras emociones, así como relacionarnos de manera efectiva con los demás.",
+        autor: "David Rodríguez",
+        nombre: "Inteligencia Emocional",
+        textColor: "white"
+    },
+    {
+        id: 8,
+        texto: "La inteligencia artificial está transformando la forma en que interactuamos con la tecnología. Desde asistentes virtuales hasta sistemas de recomendación, la IA está cada vez más presente en nuestra vida diaria.",
+        autor: "Laura Pérez",
+        nombre: "Inteligencia Artificial",
+        textColor: "white"
+    },
+    {
+        id: 9,
+        texto: "La educación es la base del progreso humano. Brindar acceso a una educación de calidad es fundamental para empoderar a las personas y construir un futuro próspero.",
+        autor: "Roberto Gómez",
+        nombre: "Educación",
+        textColor: "white"
+    },
+    {
+        id: 10,
+        texto: "El arte y la cultura son expresiones fundamentales de la identidad humana. Promover el acceso a la cultura y preservar el patrimonio artístico son aspectos clave para enriquecer nuestra sociedad.",
+        autor: "María García",
+        nombre: "Arte y Cultura",
+        textColor: "white"
+    },
+    {
+        id: 11,
+        texto: "La igualdad de género es un principio fundamental de los derechos humanos. Promover la igualdad de oportunidades y el empoderamiento de las mujeres es esencial para construir sociedades justas y equitativas.",
+        autor: "Elena Ruiz",
+        nombre: "Igualdad de Género",
+        "textColor": "white"
+    },
+    {
+        id: 12,
+        texto: "La ciencia y la tecnología son motores del progreso humano. Fomentar la investigación y la innovación es clave para resolver los desafíos globales y mejorar la calidad de vida de las personas.",
+        autor: "Jorge Martínez",
+        nombre: "Ciencia y Tecnología",
+        textColor: "white"
+    },
+    {
+        id: 13,
+        texto: "El respeto por la diversidad cultural enriquece nuestras sociedades y promueve la convivencia pacífica. Valorar las diferencias y promover la inclusión son pilares de una sociedad democrática y tolerante.",
+        autor: "Ana López",
+        nombre: "Diversidad Cultural",
+        textColor: "white"
+    },
+    {
+        id: 14,
+        texto: "La salud mental es tan importante como la salud física. Promover el bienestar emocional y brindar acceso a servicios de salud mental es fundamental para una sociedad saludable y equilibrada.",
+        autor: "Mario González",
+        nombre: "Salud Mental",
+        textColor: "white"
+    },
+    {
+        id: 15,
+        texto: "La innovación tecnológica está impulsando la transformación digital en todos los sectores. Desde la industria manufacturera hasta los servicios financieros, la digitalización está cambiando la forma en que trabajamos y vivimos.",
+        autor: "Laura Gómez",
+        nombre: "Transformación Digital",
+        textColor: "white"
+    },
+    {
+        id: 16,
+        texto: "La justicia social es un principio fundamental para construir sociedades inclusivas y equitativas. Promover la igualdad de oportunidades y combatir la discriminación son pasos clave hacia un mundo más justo.",
+        autor: "Carlos Sánchez",
+        nombre: "Justicia Social",
+        textColor: "white"
+    },
+    {
+        id: 17,
+        texto: "La cooperación internacional es esencial para abordar los desafíos globales. Trabajar juntos en áreas como el cambio climático, la seguridad alimentaria y la salud pública puede llevar a soluciones más efectivas y sostenibles.",
+        autor: "Luis Martínez",
+        nombre: "Cooperación Internacional",
+        textColor: "white"
+    },
+    {
+        id: 18,
+        texto: "La ética en la inteligencia artificial es un tema crucial en la era digital. Garantizar que los sistemas de IA sean justos, transparentes y responsables es fundamental para proteger los derechos y la dignidad de las personas.",
+        autor: "Elena García",
+        nombre: "Ética en la IA",
+        textColor: "white"
+    },
+    {
+        id: 19,
+        texto: "El voluntariado es una forma poderosa de contribuir al bienestar de la sociedad. El tiempo y los esfuerzos dedicados por los voluntarios tienen un impacto significativo en comunidades de todo el mundo.",
+        autor: "Andrés López",
+        nombre: "Voluntariado",
+        textColor: "white"
+    }
+]  
--- src/stores/Phrases.ts
+++ src/stores/Phrases.ts
@@ -1,35 +1,37 @@
 import { create } from "zustand";
 import type { Tema } from "../types/types";
+import { questionPlay } from "../../api/data";
 
 interface PhrasesState {
   texto: string | TrustedHTML;
   phrases: Tema[];
   currentPhrase: number;
-  textColor: string; 
-  typedLetters: { letter: string; correct: boolean }[]; 
+  textColor: string;
+  typedLetters: { letter: string; correct: boolean }[];
   fetchPhrases: (limit: number) => Promise<void>;
-  setTextColor: (color: string) => void; 
+  setTextColor: (color: string) => void;
+  setPhrase: (phrase: string) => void;
 }
 
 export const usePhrasesStore = create<PhrasesState>((set) => {
   return {
+    texto: "",
     phrases: [],
     currentPhrase: 0,
     textColor: "white",
-    typedLetters: [], 
+    typedLetters: [],
+    setPhrase: (phrase: string) => {
+      set({ texto: phrase });
+    },
 
     fetchPhrases: async (limit: number) => {
-      set({
-        phrases: [
-          {
-            id: 1,
-            texto: "La tecnología avanza a pasos agigantados, transformando nuestra forma de vivir y trabajar. La inteligencia artificial, en particular, está revolucionando múltiples industrias al ofrecer soluciones innovadoras y eficientes.",
-            autor: "John Smith",
-            nombre: "Avance Tecnológico",
-            textColor: undefined
-          },
-        ],
-      });
+      try {
+        const randomIndex = Math.floor(Math.random() * questionPlay.length);
+        const randomPhrase = questionPlay[randomIndex];
+        set({ phrases: [randomPhrase] });
+      } catch (error) {
+        console.error("Error fetching phrases:", error);
+      }
     },
 
     setTextColor: (color: string) => {
