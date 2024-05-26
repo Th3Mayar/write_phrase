326972808d6dcee2ec9e2083bcb6d64f25850ed5
José F. Henriquez <108672422+Th3Mayar@users.noreply.github.com>
2024-05-26 19:13:14 -0400

    fix: delete .vercel/output directory to solution

--- .vercel/output/functions/_render.func/home/jfrancisco/Repos/projects/write_phrase/.vercel/output/_functions/chunks/game_B-JwBmra.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/game_CIbluKS7.mjs').then(n => n.g);
-
-export { page };
--- .vercel/output/functions/_render.func/home/jfrancisco/Repos/projects/write_phrase/.vercel/output/_functions/chunks/index_T5B2x9Ft.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_CiEPz0QI.mjs');
-
-export { page };
